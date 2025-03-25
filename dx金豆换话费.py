c41c6d890f794081de70997b4cb95dc1a82f8e4f
青城 <118874466+qingcheng2022@users.noreply.github.com>
2025-03-25 16:37:08 +0800

    Update and rename Ruishu.py to dx金豆换话费.py

--- dx金豆换话费.py
+++ dx金豆换话费.py
@@ -1,3 +1,9 @@
+
+'''
+变量：chinaTelecomAccount
+变量格式: 手机号#服务密码
+多号创建多个变量&隔开
+'''
 import requests
 import re
 import time
@@ -28,8 +34,8 @@ class BlockAll(cookiejar.CookiePolicy):
     return_ok = set_ok = domain_return_ok = path_return_ok = lambda self, *args, **kwargs: False
     netscape = True
     rfc2965 = hide_cookie2 = False
-    
-def printn(m):  
+
+def printn(m):
     print(f'\n{m}')
 ORIGIN_CIPHERS = ('DEFAULT@SECLEVEL=1')
 
@@ -44,17 +50,17 @@ class DESAdapter(HTTPAdapter):
         CIPHERS = ':'.join(CIPHERS)
         self.CIPHERS = CIPHERS + ':!aNULL:!eNULL:!MD5'
         super().__init__(*args, **kwargs)
- 
+
     def init_poolmanager(self, *args, **kwargs):
         context = create_urllib3_context(ciphers=self.CIPHERS)
         kwargs['ssl_context'] = context
         return super(DESAdapter, self).init_poolmanager(*args, **kwargs)
- 
+
     def proxy_manager_for(self, *args, **kwargs):
         context = create_urllib3_context(ciphers=self.CIPHERS)
         kwargs['ssl_context'] = context
         return super(DESAdapter, self).proxy_manager_for(*args, **kwargs)
- 
+
 
 requests.packages.urllib3.disable_warnings()
 ssl_context = ssl.create_default_context()
@@ -63,8 +69,8 @@ ssl_context.verify_mode = ssl.CERT_NONE
 ssl_context.set_ciphers('DEFAULT@SECLEVEL=0')
 ss = requests.session()
 ss.ssl=ssl_context
-ss.headers={"User-Agent":"Mozilla/5.0 (Linux; Android 13; 22081212C Build/TKQ1.220829.002) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.97 Mobile Safari/537.36","Referer":"https://wapact.189.cn:9001/JinDouMall/JinDouMall_independentDetails.html"}    
-ss.mount('https://', DESAdapter())       
+ss.headers={"User-Agent":"Mozilla/5.0 (Linux; Android 13; 22081212C Build/TKQ1.220829.002) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.97 Mobile Safari/537.36","Referer":"https://wapact.189.cn:9001/JinDouMall/JinDouMall_independentDetails.html"}
+ss.mount('https://', DESAdapter())
 yc = 0.1
 wt = 0
 kswt = -3
@@ -81,7 +87,7 @@ except:
     dhjl = {}
 if yf not in dhjl:
     dhjl[yf] = {}
-        
+
 
 
 
@@ -122,15 +128,15 @@ MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC+ugG5A8cZ3FqUKDwM57GM4io6JGcStivT8UdGt67P
 -----END PUBLIC KEY-----'''
 
 
-def t(h):    
+def t(h):
     date = datetime.datetime.now()
     date_zero = datetime.datetime.now().replace(year=date.year, month=date.month, day=date.day, hour=h, minute=59, second=59)
     date_zero_time = int(time.mktime(date_zero.timetuple()))
     return date_zero_time
-    
 
 
-def encrypt(text):    
+
+def encrypt(text):
     cipher = DES3.new(key, DES3.MODE_CBC, iv)
     ciphertext = cipher.encrypt(pad(text.encode(), DES3.block_size))
     return ciphertext.hex()
@@ -140,22 +146,22 @@ def decrypt(text):
     cipher = DES3.new(key, DES3.MODE_CBC, iv)
     plaintext = unpad(cipher.decrypt(ciphertext), DES3.block_size)
     return plaintext.decode()
-    
-    
-    
+
+
+
 def b64(plaintext):
     public_key = RSA.import_key(public_key_b64)
     cipher = PKCS1_v1_5.new(public_key)
     ciphertext = cipher.encrypt(plaintext.encode())
     return base64.b64encode(ciphertext).decode()
-    
+
 def encrypt_para(plaintext):
     public_key = RSA.import_key(public_key_data)
     cipher = PKCS1_v1_5.new(public_key)
     ciphertext = cipher.encrypt(plaintext.encode())
     return ciphertext.hex()
-  
-            
+
+
 def encode_phone(text):
     encoded_chars = []
     for char in text:
@@ -166,49 +172,49 @@ def ophone(t):
     key = b'34d7cb0bcdf07523'
     utf8_key = key.decode('utf-8')
     utf8_t = t.encode('utf-8')
-    cipher = AES.new(key, AES.MODE_ECB) 
-    ciphertext = cipher.encrypt(pad(utf8_t, AES.block_size)) 
-    return ciphertext.hex() 
+    cipher = AES.new(key, AES.MODE_ECB)
+    ciphertext = cipher.encrypt(pad(utf8_t, AES.block_size))
+    return ciphertext.hex()
 
 def send(uid,content):
-    r = requests.post('https://wxpusher.zjiecode.com/api/send/message',json={"appToken":"AT_3hr0wdZn5QzPNBbpTHFXawoDIsSUmPkN","content":content,"contentType":1,"uids":[uid]}).json()
+    r = requests.post('https://wxpusher.zjiecode.com/api/send/message',json={"appToken":appToken,"content":content,"contentType":1,"uids":[uid]}).json()
     return r
-    
-            
+
+
 def userLoginNormal(phone,password):
     alphabet = 'abcdef0123456789'
     uuid = [''.join(random.sample(alphabet, 8)),''.join(random.sample(alphabet, 4)),'4'+''.join(random.sample(alphabet, 3)),''.join(random.sample(alphabet, 4)),''.join(random.sample(alphabet, 12))]
     timestamp=datetime.datetime.now().strftime("%Y%m%d%H%M%S")
     loginAuthCipherAsymmertric = 'iPhone 14 15.4.' + uuid[0] + uuid[1] + phone + timestamp + password[:6] + '0$$$0.'
-    
+
     r = ss.post('https://appgologin.189.cn:9031/login/client/userLoginNormal',json={"headerInfos": {"code": "userLoginNormal", "timestamp": timestamp, "broadAccount": "", "broadToken": "", "clientType": "#9.6.1#channel50#iPhone 14 Pro Max#", "shopId": "20002", "source": "110003", "sourcePassword": "Sid98s", "token": "", "userLoginName": phone}, "content": {"attach": "test", "fieldData": {"loginType": "4", "accountType": "", "loginAuthCipherAsymmertric": b64(loginAuthCipherAsymmertric), "deviceUid": uuid[0] + uuid[1] + uuid[2], "phoneNum": encode_phone(phone), "isChinatelecom": "0", "systemVersion": "15.4.0", "authentication": password}}}).json()
-    
-    
-    
+
+
+
     l = r['responseData']['data']['loginSuccessResult']
-    
+
     if l:
         load_token[phone] = l
         with open(load_token_file, 'w') as f:
             json.dump(load_token, f)
-        ticket = get_ticket(phone,l['userId'],l['token']) 
+        ticket = get_ticket(phone,l['userId'],l['token'])
         return ticket
-       
+
     return False
 def get_ticket(phone,userId,token):
     r = ss.post('https://appgologin.189.cn:9031/map/clientXML',data='<Request><HeaderInfos><Code>getSingle</Code><Timestamp>'+datetime.datetime.now().strftime("%Y%m%d%H%M%S")+'</Timestamp><BroadAccount></BroadAccount><BroadToken></BroadToken><ClientType>#9.6.1#channel50#iPhone 14 Pro Max#</ClientType><ShopId>20002</ShopId><Source>110003</Source><SourcePassword>Sid98s</SourcePassword><Token>'+token+'</Token><UserLoginName>'+phone+'</UserLoginName></HeaderInfos><Content><Attach>test</Attach><FieldData><TargetId>'+encrypt(userId)+'</TargetId><Url>4a6862274835b451</Url></FieldData></Content></Request>',headers={'user-agent': 'CtClient;10.4.1;Android;13;22081212C;NTQzNzgx!#!MTgwNTg1'})
 
     #printn(phone, '获取ticket', re.findall('<Reason>(.*?)</Reason>',r.text)[0])
-    
+
     tk = re.findall('<Ticket>(.*?)</Ticket>',r.text)
-    if len(tk) == 0:        
+    if len(tk) == 0:
         return False
-    
-    
+
+
     return decrypt(tk[0])
 
 
-        
+
 def queryInfo(phone,s):
     global rs
     a = 1
@@ -218,7 +224,7 @@ def queryInfo(phone,s):
             ck[bd[0]] = bd[1]
 
         r = s.get('https://wapact.189.cn:9001/gateway/golden/api/queryInfo',cookies=ck).json()
-        
+
         try:
             printn(f'{phone} 金豆余额 {r["biz"]["amountTotal"]}')
             amountTotal=  r["biz"]["amountTotal"]
@@ -228,76 +234,76 @@ def queryInfo(phone,s):
             if rs == 1:
                 bd = js.call('main').split('=')
                 ck [bd[0]] = bd[1]
-                
 
-            res = s.post('http://wapact.189.cn:9000/gateway/stand/detail/exchange',json={"activityId":jdaid},cookies=ck).text            
-            
+
+            res = s.post('http://wapact.189.cn:9000/gateway/stand/detail/exchange',json={"activityId":jdaid},cookies=ck).text
+
             if '$_ts=window' in res:
                 first_request()
                 rs = 1
-            
+
             time.sleep(3)
         else:
             return r
         a += 1
-        
-    
+
+
     return r
-    
+
 
 def exchange(phone,s,title,aid, uid):
-    
+
     try:
         bd = js.call('main').split('=')
         ck [bd[0]] = bd[1]
-        r = s.post('https://wapact.189.cn:9001/gateway/stand/detailNew/exchange',json={"activityId":aid},cookies=ck)
+        r = s.post('https://wapact.189.cn:9001/gateway/standExchange/detailNew/exchange',json={"activityId":aid},cookies=ck)
         printn(f"响应码: {r.status_code}")
-        
+
         if '$_ts=window' in r.text:
-            
+
             first_request(r.text)
             return
-        r = r.json()        
-        
+        r = r.json()
+
         if r["code"] == 0:
             if r["biz"] != {} and r["biz"]["resultCode"] in errcode:
                 #printn(str(datetime.datetime.now())[11:22], phone, title,errcode[r["biz"]["resultCode"]])
                 printn(f'{str(datetime.datetime.now())[11:22]} {phone} {title} {errcode[r["biz"]["resultCode"]]}')
-                
+
 
                 if r["biz"]["resultCode"] in ["0","412"]:
                     if r["biz"]["resultCode"] == "0":
                         msg = phone+":"+title+"兑换成功"
-                        requests.post('http://106.53.145.222:81/work.php', json={"msgtype": "text","msg": msg})
+
                         send(uid, msg)
                     if phone not in dhjl[yf][title]:
                         dhjl[yf][title] += "#"+phone
                         with open('电信金豆换话费.log', 'w') as f:
                             json.dump(dhjl, f, ensure_ascii=False)
-                            
-            
+
+
         else:
             #printn(str(datetime.datetime.now())[11:22], phone, r["message"])
             printn(f'{str(datetime.datetime.now())[11:22]} {phone} {r}')
-            
+
     except Exception as e:
         #print(e)
         pass
-    
-    
+
+
 def dh(phone,s,title,aid,wt, uid):
 
     while wt > time.time():
         pass
-    
+
     printn(f"{str(datetime.datetime.now())[11:22]} {phone} {title} 开始兑换")
-    
+
     if rs:
         bd = js.call('main').split('=')
         ck [bd[0]] = bd[1]
     for cs in range(cfcs):
-        threading.Thread(target=exchange,args=(phone,s,title,aid, uid)).start()    
-        #time.sleep(5)  
+        threading.Thread(target=exchange,args=(phone,s,title,aid, uid)).start()
+        #time.sleep(5)
 
 
 
@@ -308,44 +314,59 @@ def lottery(s):
                 bd = js.call('main').split('=')
                 ck [bd[0]] = bd[1]
             else:
-                cookie = {} 
+                cookie = {}
             r = s.post('https://wapact.189.cn:9001/gateway/golden/api/lottery',json={"activityId":"6384b49b1e44396da4f1e4a3"},cookies=ck)
         except:
-            pass 
-        time.sleep(3)    
-    
+            pass
+        time.sleep(3)
 
+def aes_ecb_encrypt(plaintext, key):
+    key = key.encode('utf-8')
+    if len(key) not in [16, 24, 32]:
+        raise ValueError("密钥长度必须为16/24/32字节")
+
+    # 对明文进行PKCS7填充
+    padded_data = pad(plaintext.encode('utf-8'), AES.block_size)
+    #padded_data = plaintext.encode('utf-8')
+    # 创建AES ECB加密器
+    cipher = AES.new(key, AES.MODE_ECB)
+
+    # 加密并返回Base64编码结果
+    ciphertext = cipher.encrypt(padded_data)
+    return base64.b64encode(ciphertext).decode('utf-8')
 def ks(phone, ticket, uid):
     global wt
-    
+
     wxp[phone] = uid
     s = requests.session()
     s.headers={"User-Agent":"Mozilla/5.0 (Linux; Android 13; 22081212C Build/TKQ1.220829.002) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.97 Mobile Safari/537.36","Referer":"https://wapact.189.cn:9001/JinDouMall/JinDouMall_independentDetails.html"}
     s.cookies.set_policy(BlockAll())
-    s.mount('https://', DESAdapter())  
-    s.timeout = 30  
+    s.mount('https://', DESAdapter())
+    s.timeout = 30
     if rs:
         bd = js.call('main').split('=')
         ck [bd[0]] = bd[1]
 
-    
-    login = s.post('https://wapact.189.cn:9001/unified/user/login',json={"ticket":ticket,"backUrl":"https%3A%2F%2Fwapact.189.cn%3A9001","platformCode":"P201010301","loginType":2}, cookies=ck).json()
+    data = aes_ecb_encrypt(json.dumps({"ticket":ticket,"backUrl":"https%3A%2F%2Fwapact.189.cn%3A9001","platformCode":"P201010301","loginType":2}), 'telecom_wap_2018')
+
+    login = ss.post('https://wapact.189.cn:9001/unified/user/login',data=data, headers={"Content-Type":"application/json;charset=UTF-8","Accept":"application/json, text/javascript, */*; q=0.01"}, cookies=ck).json()
+    # login = s.post('https://wapact.189.cn:9001/unified/user/login',json={"ticket":ticket,"backUrl":"https%3A%2F%2Fwapact.189.cn%3A9001","platformCode":"P201010301","loginType":2}, cookies=ck).json()
     if login['code'] == 0:
         printn(phone+" 获取token成功")
-        s.headers["Authorization"] = "Bearer " + login["biz"]["token"]   
+        s.headers["Authorization"] = "Bearer " + login["biz"]["token"]
 
         queryInfo(phone,s)
-        
+
 
         if rs:
             bd = js.call('main').split('=')
             ck [bd[0]] = bd[1]
-        
-        queryBigDataAppGetOrInfo = s.get('https://wapact.189.cn:9001/gateway/golden/api/queryBigDataAppGetOrInfo?floorType=0&userType=1&page&1&order=2&tabOrder=',cookies=ck).json()
-        #printn(queryBigDataAppGetOrInfo)
+
+        queryBigDataAppGetOrInfo = s.get('https://waphub.189.cn/gateway/golden/goldGoods/getGoodsList??floorType=0&userType=1&page&1&order=2&tabOrder=',cookies=ck).json()
+        # printn(queryBigDataAppGetOrInfo)
         for i in queryBigDataAppGetOrInfo["biz"]["ExchangeGoodslist"]:
             if '话费' not in i["title"]:continue
-            
+
             if '0.5元' in i["title"] or '5元' in  i["title"]:
                 jp["9"][i["title"]] = i["id"]
             elif '1元' in i["title"] or '10元' in i["title"]:
@@ -353,52 +374,52 @@ def ks(phone, ticket, uid):
             else:
                 jp["12"][i["title"]] = i["id"]
 
-                
+
 
         h = datetime.datetime.now().hour
         if  11 > h > 1:
             h = 9
-            
-        elif 23 > h > 1:            
+
+        elif 23 > h > 1:
             h = 13
-            
+
         else:
             h = 23
-        
+
         if len(sys.argv) ==2:
             h = int(sys.argv[1])
         #h=23
         d = jp[str(h)]
-        
+
         wt = t(h) + kswt
-        
+
         if jp["12"] != {}:
             d.update(jp["12"])
             wt = 0
-        
+
         for di in d:
             #if '5' in di:
             if di not in dhjl[yf]:
                 dhjl[yf][di] = ""
             if phone in dhjl[yf][di] :
                 printn(f"{phone} {di} 已兑换")
-                
+
             else:
 
                 printn(f"{phone} {di}")
                 if wt - time.time() > 20 * 60:
                     print("等待时间超过20分钟")
                     return
-                
-                
+
+
                 threading.Thread(target=dh,args=(phone,s,di,d[di],wt, uid)).start()
-            
-            
+
+
     else:
-        
+
         printn(f"{phone} 获取token {login['message']}")
-       
-       
+
+
 
 def first_request(res=''):
     global js, fw
@@ -411,28 +432,29 @@ def first_request(res=''):
     for script in scripts:
         if 'src' in str(script):
             rsurl =  re.findall('src="([^"]+)"', str(script))[0]
-            
+
         if '$_ts=window' in script.get_text():
             ts_code = script.get_text()
-            
-    
+
+
     urls  = url.split('/')
     rsurl = urls[0] + '//' + urls[2] + rsurl
-    #print(rsurl)    
+    #print(rsurl)
     ts_code += ss.get(rsurl).text
     content_code = soup.find_all('meta')[1].get('content')
     with open("瑞数通杀.js") as f:
         js_code_ym = f.read()
     js_code = js_code_ym.replace('content_code', content_code).replace("'ts_code'", ts_code)
-    js = execjs.compile(js_code) 
+    js = execjs.compile(js_code)
 
     for cookie in ss.cookies:
         ck[cookie.name] = cookie.value
     return content_code, ts_code, ck
 
-    
-    
+
+
 def main():
+    print()
     global wt,rs
     r = ss.get('https://wapact.189.cn:9001/gateway/stand/detailNew/exchange')
     if '$_ts=window' in r.text:
@@ -441,40 +463,43 @@ def main():
         first_request()
     else:
         print("瑞数加密已关闭")
-        rs = 0     
-    if os.environ.get('jdhf')!= None:
-        chinaTelecomAccount = os.environ.get('jdhf')
+        rs = 0
+    if os.environ.get('chinaTelecomAccount')!= None:
+        chinaTelecomAccount = os.environ.get('chinaTelecomAccount')
     else:
-       chinaTelecomAccount = jdhf
-    
+        chinaTelecomAccount = jdhf
+
     for i in chinaTelecomAccount.split('&'):
 
-        i = i.split('@')
+        i = i.split('#')
         phone = i[0]
         password = i[1]
         uid = i[-1]
-        ticket = False 
-        
-        #ticket = get_userTicket(phone)  
-        
+        ticket = False
+
+        #ticket = get_userTicket(phone)
+
         if phone in load_token:
             printn(f'{phone} 使用缓存登录')
             ticket = get_ticket(phone,load_token[phone]['userId'],load_token[phone]['token'])
-        
+
         if ticket == False:
             printn(f'{phone} 使用密码登录')
             ticket = userLoginNormal(phone,password)
-           
+
         if ticket:
             threading.Thread(target=ks,args=(phone, ticket, uid)).start()
-            
+
             time.sleep(1)
         else:
             printn(f'{phone} 登录失败')
-        
 
+#手机号@密码@wxpusheruid
 jdhf = ""
-cfcs = 5
+#重发次数
+cfcs = 15
+#wxpusher推送appToken
+appToken = ""
 jdaid = '60dd79533dc03d3c76bdde30'
 ck = {}
 load_token_file = 'chinaTelecom_cache.json'
@@ -484,4 +509,4 @@ try:
 except:
     load_token = {}
 
-main()        
+main()
